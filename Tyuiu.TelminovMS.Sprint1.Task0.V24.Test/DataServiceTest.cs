6181c660c8b7c5a2649bb02c9190421331b81ce8
TelminovMS <mtelminov02@gmail.com>
2026-09-17 16:46:29 +0500

    Add project files.

---
+++ Tyuiu.TelminovMS.Sprint1.Task0.V24.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.TelminovMS.Sprint1.Task0.V24.Lib;
+
+namespace Tyuiu.TelminovMS.Sprint1.Task0.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
