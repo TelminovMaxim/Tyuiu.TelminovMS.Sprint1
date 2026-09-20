cd425986995440e9692838aa8b1a76021cffa15e
Sie-mt <mtelminov02@gmail.com>
2026-09-21 00:16:07 +0500

    Task1

---
+++ Tyuiu.TelminovMS.Sprint1.Task1.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.TelminovMS.Sprint1.Task1.V5.Lib;
+
+namespace Tyuiu.TelminovMS.Sprint1.Task1.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 3.0;
+            double y = 5.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(10, res);
+        }
+    }
+}
