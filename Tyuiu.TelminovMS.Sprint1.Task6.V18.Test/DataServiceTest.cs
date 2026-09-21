9bc87fc191079a6a13af911868e14d7ee10fa7de
Sie-mt <mtelminov02@gmail.com>
2026-09-21 18:10:58 +0500

    Task6

---
+++ Tyuiu.TelminovMS.Sprint1.Task6.V18.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.TelminovMS.Sprint1.Task6.V18.Lib;
+
+namespace Tyuiu.TelminovMS.Sprint1.Task6.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            string strTest = "123";
+
+            bool res = ds.CheckNumber(strTest);
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.TelminovMS.Sprint1.Task6.V18/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.TelminovMS.Sprint1.Task6.V18.Lib;
+
+namespace Tyuiu.TelminovMS.Sprint1.Task6.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Тельминов М. С. | ИСТНб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Работа со строками класс String                                   *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Тельминов Максим Сергеевич | ИСТНб-26-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст. Проверить, что строка    *");
+            Console.WriteLine("* является символьным представлением натурального числа.                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите строку: ");
+            string input = Console.ReadLine();
+
+            bool result = ds.CheckNumber(input);
+
+            if (result)
+            {
+                Console.WriteLine($"Строка '{input}' является символьным представлением натурального числа.");
+            }
+            else
+            {
+                Console.WriteLine($"Строка '{input}' не является символьным представлением натурального числа.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
