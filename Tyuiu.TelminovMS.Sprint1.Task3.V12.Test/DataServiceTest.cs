2e3c698ef19301bcadd4fc727125e1128b6fc476
Sie-mt <mtelminov02@gmail.com>
2026-09-21 15:29:12 +0500

    Task3

---
+++ Tyuiu.TelminovMS.Sprint1.Task3.V12.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.TelminovMS.Sprint1.Task3.V12.Lib;
+
+namespace Tyuiu.TelminovMS.Sprint1.Task3.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+
+            double x = 3;
+            double y = 4;   
+            var res = ds.TriangleArea(x, y);
+
+            Assert.AreEqual(6, res);
+        }
+    }
+}
---
+++ Tyuiu.TelminovMS.Sprint1.Task3.V12/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.TelminovMS.Sprint1.Task3.V12.Lib;
+
+namespace Tyuiu.TelminovMS.Sprint1.Task3.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Тельминов М. С. | ИСТНб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Тельминов Максим Сергеевич | ИСТНб-26-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу для вычисления площади прямоугольного треугольника   *");
+            Console.WriteLine("* по длинам двух катетов                                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double lengthCathetus1, lengthCathetus2;
+
+            Console.WriteLine("Введите длину первого катета: ");
+            lengthCathetus1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите длину второго катета: ");
+            lengthCathetus2 = Convert.ToDouble(Console.ReadLine());
+
+            var res = ds.TriangleArea(lengthCathetus1, lengthCathetus2);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Площадь прямоугольного треугольника: {res}");
+
+            Console.ReadKey();
+        }
+    }
+}
